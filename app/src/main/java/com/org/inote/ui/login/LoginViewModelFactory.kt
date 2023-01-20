416ea797ccf437d14b018bc36fa9322f903d916e
Gilbert TURYABASA <gilbert.turyabasa@engie.com>
2023-01-20 16:38:55 +0300

    second Commit

---
+++ app/src/main/java/com/org/inote/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.org.inote.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.org.inote.data.LoginDataSource
+import com.org.inote.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
